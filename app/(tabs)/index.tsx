44c7e892db69e05d5ee4bb6d7bfb3614320d1641
jody816 <81346079+jody816@users.noreply.github.com>
2024-12-29 15:08:30 +0900

    fix: 루트 파일 위치 수정(예시)

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>뀨뀨뀨</Text>
+      <Link href="/about" style={styles.button}>
+        뀨
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
